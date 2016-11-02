be130f01236f96de45cf2534701dca68fa271c8f
Frank Staude <frank@staude.net>
2016-11-02 15:43:10 +0100

    Altersstufe automatisch setzten in Abhängigkeit der Bildungsstufen

--- js/materialpool.js
+++ js/materialpool.js
@@ -29,5 +29,56 @@ jQuery(document).ready(function(){
     }
 });
 
+/**
+ *
+ * Set Alterstufen depends on Bildungsstufe
+ *
+ */
+
+jQuery(document).ready(function(){
+    jQuery(".pods-form-ui-field-name-pods-meta-material-bildungsstufe").click( function() {
+        switch ( jQuery(this).val() ) {
+            case "11":    // Elementarbereich
+                set_altersstufe( 4 ); // 1-5
+                break;
+            case "12":    // Erwachsenenbildung
+                set_altersstufe( 9 ); // 1-5
+                break;
+            case "13":    // Kinder und Jugendarbeit
+                set_altersstufe( 5 ); // 5-10
+                set_altersstufe( 6 ); // 10-13
+                set_altersstufe( 7 ); // 13-15
+                set_altersstufe( 8 ); // 15-19
+                break;
+            case "14":    // Kindergottesdienst
+                set_altersstufe( 4 ); // 1-5
+                set_altersstufe( 5 ); // 5-10
+                break;
+            case "15":    // Konfirmandenarbeit
+                set_altersstufe( 7 ); // 13-15
+                break;
+            case "17":    // Berufsshule
+                set_altersstufe( 8 ); // 15-19
+                break;
+            case "18":    // Grundschule
+                set_altersstufe( 5 ); // 5-10
+                break;
+            case "19":    // Oberstufe
+                set_altersstufe( 8 ); // 15-19
+                break;
+            case "20":    // Sekundarstufe
+                set_altersstufe( 6 ); // 10-13
+                set_altersstufe( 7 ); // 13-15
+                break;
+        }
+    })
+});
 
 
+function set_altersstufe( id ) {
+    jQuery(".pods-form-ui-field-name-pods-meta-material-altersstufe").each( function() {
+        if ( jQuery(this).val() == id ) {
+            jQuery(this).attr('checked', true);
+        }
+    })
+}
