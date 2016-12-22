d9ca191a7dc917752741f004b881b08dc7f6582b
Frank Staude <frank@staude.net>
2016-12-22 17:48:39 +0100

    change bookmarklet
    fixes #101

--- js/bookmarklet.src.js
+++ js/bookmarklet.src.js
@@ -1,32 +1,5 @@
-(function(){
+javascript:void(window.open("http://material.rpi-virtuell.de/wp-admin/post-new.php?post_type=material&url="
+    +encodeURIComponent(escape(location.href))
+    +"&text="+encodeURIComponent(escape( document.getSelection() ))));
 
-    // the minimum version of jQuery we want
-    var v = "3.1.1";
 
-    // check prior inclusion and version
-    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-        var done = false;
-        var script = document.createElement("script");
-        script.src = "//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js";
-        script.onload = script.onreadystatechange = function(){
-            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-                done = true;
-                initMyBookmarklet();
-            }
-        };
-        document.getElementsByTagName("head")[0].appendChild(script);
-    } else {
-        initMyBookmarklet();
-    }
-
-    function initMyBookmarklet() {
-        (window.myBookmarklet = function() {
-            var mText=document.getSelection();
-            var url ="http://material.rpi-virtuell.de/wp-admin/post-new.php?post_type=material&url="
-                +encodeURIComponent(escape(location.href))
-                +"&text="+encodeURIComponent(escape(mText));
-            window.open(url, "_blank");
-        })();
-    }
-
-})();
