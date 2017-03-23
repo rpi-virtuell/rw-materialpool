842ea7086ad9e8fdc1d020f5fde08db0de783731
Frank Staude <frank@staude.net>
2017-03-23 14:52:34 +0100

    Quickedit remove fields
    fixes: #166

--- js/materialpool.js
+++ js/materialpool.js
@@ -782,3 +782,42 @@ jQuery(document).ready(function(){
 
     });
 });
+
+/*
+ * Quickedit Elemente ausblenden
+ */
+
+jQuery(document).ready( function($) {
+    $('span:contains("Altersstufen")').each(function (i) {
+        $(this).parent().remove();
+    });
+    $('span:contains("Inklusion")').each(function (i) {
+        $(this).parent().remove();
+    });
+    $('span:contains("Konfession")').each(function (i) {
+        $(this).parent().remove();
+    });
+    $('span:contains("Lizenzen")').each(function (i) {
+        $(this).parent().remove();
+    });
+    $('span:contains("Schlagworte")').each(function (i) {
+        $(this).parent().remove();
+    });
+    $('span:contains("Sprachen")').each(function (i) {
+        $(this).parent().remove();
+    });
+    $('span:contains("Verfügbarkeiten")').each(function (i) {
+        $(this).parent().remove();
+    });
+    $('span:contains("Zugänglichkeiten")').each(function (i) {
+        $(this).parent().remove();
+    });
+    $('span:contains("Bildungsstufen")').each(function (i) {
+        $(this).parent().parent().remove();
+    });
+
+    $('.inline-edit-date').each(function (i) {
+        $(this).remove();
+    });
+});
+
