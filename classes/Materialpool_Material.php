29dc44ef1c996e21abce50732eb82f420c7153ea
Frank Staude <frank@staude.net>
2017-11-13 14:53:25 +0100

    fix taxonomy template

--- classes/Materialpool_Material.php
+++ classes/Materialpool_Material.php
@@ -20,6 +20,9 @@ class Materialpool_Material {
     static public function load_template($template) {
         global $post;
 
+        if (is_tax() ) {
+	        return $template;
+        }
         if ($post->post_type == "material" && !is_embed() ){
             if ( is_single() ) {
                 if ( $theme_file = locate_template( array ( 'materialpool/single-material.php' ) ) ) {
