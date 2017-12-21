316861098548c31b79ee49d4890981e2327239e3
Frank Staude <frank@staude.net>
2017-12-21 11:52:08 +0100

    Synonyme bei Schlagworten anzeigen

--- classes/Materialpool_Schlagworte.php
+++ classes/Materialpool_Schlagworte.php
@@ -18,7 +18,8 @@ class Materialpool_Schlagworte {
      */
     static public function taxonomy_column( $columns ) {
         unset( $columns[ 'posts' ] );
-        $columns[ 'uses' ] = __( 'Anzahl', Materialpool::get_textdomain() );
+	    $columns[ 'synonym' ] = __( 'Synonyme', Materialpool::get_textdomain() );
+	    $columns[ 'uses' ] = __( 'Anzahl', Materialpool::get_textdomain() );
         return $columns;
     }
 
@@ -30,7 +31,7 @@ class Materialpool_Schlagworte {
      */
     static public function taxonomy_column_data( $out, $column_name, $term_id ) {
         global $wpdb;
-
+		$out = '';
         switch ($column_name) {
             case 'uses':
                 //$anzahl = $wpdb->get_col( $wpdb->prepare( "SELECT count( meta_id ) as anzahl  FROM  $wpdb->postmeta WHERE meta_key = %s and meta_value = %s", 'material_schlagworte', $term_id) );
@@ -39,6 +40,25 @@ class Materialpool_Schlagworte {
 	            $out .=  '<a href="' . $url . '">'.$anzahl->count . '</a>';
                 break;
 
+	        case 'synonym':
+		        $term = get_term( $term_id, 'schlagwort' );
+	        	$posts = get_posts( array(
+			        'post_type' => 'synonym',
+			        'orderby' => 'post_title',
+			        'post_status' => 'published',
+			        'meta_key' => 'normwort',
+			        'meta_value' => $term->slug,
+		        ));
+	        	$counter=0;
+	        	foreach ( $posts as $post ) {
+	        		if ( $counter > 0 ) {
+	        			$out .=  ', ';
+			        }
+			        $out .=  $post->post_title;
+	        		$counter++;
+		        }
+
+	        	break;
             default:
                 break;
         }
