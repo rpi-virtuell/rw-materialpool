702f9c0301f753d7414ff9ba28d498409113e623
Frank Staude <frank@staude.net>
2018-03-20 15:36:06 +0100

    Lizenzierung wird nicht angezeigt

--- classes/Materialpool_Material.php
+++ classes/Materialpool_Material.php
@@ -1588,7 +1588,30 @@ END;
         }
     }
 
+	/**
+	 *
+	 * @since 0.0.1
+	 * @access	public
+	 *
+	 */
+	static public function lizenz() {
+		echo Materialpool_Material::get_lizenz();
+	}
 
+	/**
+	 *
+	 * @since 0.0.1
+	 * @access	public
+	 *
+	 */
+	static public function get_lizenz() {
+		global $post;
+
+		$vid = get_metadata( 'post', $post->ID, 'material_lizenz', true );
+		if ( is_array( $vid ) ) {
+			return $vid[ 'name'];
+		}
+	}
 	/**
 	 *
 	 * @since 0.0.1
