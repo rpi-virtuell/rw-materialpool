d4caf082a1ff3ebb626e603a741722647141986b
Frank Staude <frank@staude.net>
2019-07-08 11:08:54 +0200

    Kompetenzen

---
+++ classes/Materialpool_Kompetenzen.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ * @since      0.0.1
+ * @package    Materialpool
+ * @author     Frank Staude <frank@staude.net>
+ *
+ */
+
+
+class Materialpool_Kompetenzen {
+
+	/**
+	 *
+	 * @since 0.0.1
+	 * @access	public
+	 *
+	 */
+	static public function taxonomy_column( $columns ) {
+		unset( $columns[ 'posts' ] );
+		$columns[ 'uses' ] = __( 'Anzahl', Materialpool::get_textdomain() );
+		return $columns;
+	}
+
+	/**
+	 *
+	 * @since 0.0.1
+	 * @access	public
+	 *
+	 */
+	static public function taxonomy_column_data( $out, $column_name, $term_id ) {
+		global $wpdb;
+
+		switch ($column_name) {
+			case 'uses':
+				$anzahl = $wpdb->get_col( $wpdb->prepare( "SELECT count( meta_id ) as anzahl  FROM  $wpdb->postmeta WHERE meta_key = %s and meta_value = %s", 'material_kompetenz', $term_id) );
+				$out .= $anzahl[ 0 ];
+				break;
+
+			default:
+				break;
+		}
+		return $out;
+	}
+
+}
